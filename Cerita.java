21287190065d309dc1164779b585a236916ffd58
Muhammad Risky Pratama Hermawan <64541424+RiskyHermawan@users.noreply.github.com>
2021-01-18 19:44:36 +0700

    Cerita.java

---
+++ Cerita.java
@@ -0,0 +1,14 @@
+public class Cerita {
+  String NamaCerita;
+  String TeksCerita;
+
+  public Cerita (String nama, String teks){
+    this.NamaCerita = nama;
+    this.TeksCerita = teks;
+  }
+  
+  Public void printCerita() {
+    System.out.println(this.TeksCerita);
+  }
+  
+}
