d30bb7180bacf7072a61b1a3102349e6c7916407
DaniswaraNoorFauziWicaksana <64541347+DaniswaraNoorFauziWicaksana@users.noreply.github.com>
2021-01-20 01:41:09 +0700

    Update Pilihan.java

--- Pilihan.java
+++ Pilihan.java
@@ -1,1 +1,17 @@
+public class Pilihan {
+    String BlockCerita;
+    String pilih;
+    String pilihan;
+    String hasil;
 
+   public Pilihan (String BlockCerita, String pilih, String pilihan, String hasil) {
+        this.BlockCerita = BlockCerita;
+        this.pilih = pilih;
+        this.pilihan = pilihan;
+        this.hasil = hasil;
+    }
+
+    public void printPilihan () {
+        System.out.println(this.pilih + ": " + this.pilihan);
+    }
+}
