fe75e67f4cb9d96a4d8b267c9923345ad87534fc
Muhammad Risky Pratama Hermawan <64541424+RiskyHermawan@users.noreply.github.com>
2021-01-21 22:18:53 +0700

    Update and rename Cerita.java to Story.java

---
+++ Story.java
@@ -0,0 +1,14 @@
+public class Story {
+  String storyName;
+  String storyText;
+
+  public Story (String name, String text){
+    this.storyName = name;
+    this.atoryText = text;
+  }
+  
+  public void printStory(){
+    Syste.out.println(this.storyText);
+  }
+  
+}
